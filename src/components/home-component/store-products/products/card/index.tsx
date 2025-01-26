bbd9a448dc909f7ebf6b45903016294f08325fe3
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2025-01-26 14:43:26 +0500

    LIke tugadi

--- src/components/home-component/store-products/products/card/index.tsx
+++ src/components/home-component/store-products/products/card/index.tsx
@@ -1,7 +1,8 @@
 import type { FC } from "react";
-import type { CartType } from "../../../../../@types";
+import type { AuthUser, CartType } from "../../../../../@types";
 import { useNavigate } from "react-router-dom";
 import {
+  HeartFilled,
   HeartOutlined,
   SearchOutlined,
   ShoppingCartOutlined,
@@ -9,14 +10,27 @@ import {
 import { useReduxDispatch } from "../../../../../hooks/useRedux";
 import { getProductShop } from "../../../../../redux/shop-slice";
 import { notificationApi } from "../../../../../generic/notification";
-import { useIsAuthenticated } from "react-auth-kit";
+import { useAuthHeader, useAuthUser, useIsAuthenticated } from "react-auth-kit";
 import { setAuthorizationModalVisibility } from "../../../../../redux/modal-slice";
+import { useHandler } from "../../../../../generic/hendler/inde";
 
 const Card: FC<CartType> = (props) => {
+  const style_icons: string =
+    "bg-[#FFFFFF] w-[35px] h-[35px] flex rounded-lg justify-center items-center  cursor-pointer text-[20px]";
   const navigate = useNavigate();
   const dispatch = useReduxDispatch();
   const notifiy = notificationApi();
+
   const isAuth = useIsAuthenticated()();
+
+  const auth: AuthUser = useAuthUser()() ?? {};
+  const { likeHandler } = useHandler();
+  let findLikeData = auth.wishlist?.filter(
+    (value) => value.flower_id === String(props._id)
+  )[0];
+
+  let isLiked = Boolean(findLikeData);
+  console.log(isLiked);
   return (
     <div className="flex flex-col gap-[0.6rem]">
       <div className="h-[30rem] bg-[#f5f5f5] flex justify-center items-center transition-all duration-700  relative group">
@@ -31,21 +45,31 @@ const Card: FC<CartType> = (props) => {
           >
             <ShoppingCartOutlined />
           </button>
-          <button
-            onClick={() =>
+          <div
+            onClick={() => {
               isAuth
-                ? ""
+                ? likeHandler({
+                    isLiked,
+                    data: {
+                      route_path: props.category,
+                      flower_id: props._id,
+                    },
+                  })()
                 : dispatch(
                     setAuthorizationModalVisibility({
                       open: true,
                       loading: false,
                     })
-                  )
-            }
-            className="text-[1.8rem] text-[#3D3D3D] hover:text-[#46A358] w-[3.5rem] h-[3.5rem] bg-[#ffff] rounded-[0.4rem] flex items-center justify-center"
+                  );
+            }}
+            className={style_icons}
           >
-            <HeartOutlined />
-          </button>
+            {isLiked ? (
+              <HeartFilled className="text-[22px] text-[red]" />
+            ) : (
+              <HeartOutlined className="text-[22px]" />
+            )}
+          </div>
           <button
             onClick={() => navigate(`/shop/${props.category}/${props._id}`)}
             className="text-[1.8rem] text-[#3D3D3D] hover:text-[#46A358] w-[3.5rem] h-[3.5rem] bg-[#ffff] rounded-[0.4rem] flex items-center justify-center"
